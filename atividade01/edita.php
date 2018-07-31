014f24934a720599ac6630de66cf1aeebcf7f95b
1714290021 <1714290021@SJB6-D22.academico.iesb.br>
2018-07-30 21:06:57 -0300

    Atividade 01

---
+++ atividade01/edita.php
@@ -0,0 +1,19 @@
+<?php require 'cabecalho.php' ?>
+
+<h1>Edição de Aluno</h1>
+
+<form action="edita-aluno.php">
+      <input type="hidden" name="id" value="<?=$aluno['id']?>">
+	<div>
+		<label>Matrícula:</label>
+		<input type="text" name="matricula" value="<?=$aluno['matricula']?>"size="15">
+	</div>
+	<div>
+		<label>Nome:</label>
+		<input type="text" name="nome" value="<?=$aluno['nome']?>" size="30">
+	</div>
+	<input type="submit" value="Salvar"/>
+	<a href="lista-aluno.php">Desistir</a>
+</form>
+
+<?php require 'rodape.php' ?>
