c113ad7385202188685b037d5027316c9128e5bf
Felipe <inouefy@gmail.com>
2018-09-24 20:47:25 -0300

    atividade06

---
+++ atividade06/resources/views/editar.blade.php
@@ -0,0 +1,29 @@
+@extends('leiaute')
+
+@section('titulo', 'Editar Aluno')
+
+@section('conteudo')
+<form action="/aluno/salvar" class="form-horizontal" method="post">
+    {{ csrf_field() }}
+    <input type="hidden" name="id" size="15" value="{{ $aluno->id }}"/>
+    <div class="">
+        <label class="control-label">Matrícula</label>
+        <div class="">
+            <input type="text" name="matricula" size="15" value="{{ $aluno->matricula }}"/>
+        </div>
+    </div>
+    <div class="">
+        <label class="control-label">Nome</label>
+        <div class="">
+            <input type="text" name="nome" size="40" value="{{ $aluno->nome }}"/>
+        </div>
+    </div>
+    <div class="">
+        <label class="control-label"></label>
+        <div class="">
+            <button type="submit" class="btn">Salvar</button>
+            <a href="/aluno/listar">Desistir</a>
+        </div>
+    </div>
+</form>
+@endsection
---
+++ atividade06/resources/views/leiaute.blade.php
@@ -0,0 +1,11 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Sistema Academico</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+</head>
+<body>
+    <h1>@yield('titulo')</h1>
+    @yield('conteudo')
+</body>
+</html>
